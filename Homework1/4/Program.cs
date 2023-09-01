d1c0b6b51d93fb4f7f0acc60732243f5a37aa782
Dima <d3i9m9a2015s@gmail.com>
2023-09-01 17:09:21 +0900

    сделал дз1

---
+++ Homework1/4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num3 = 0;
+int num2 = num1 / 2;
+Console.WriteLine("Чётные числа: ");
+while (num2 >= 1){
+    num3 = num3 + 2;
+    num2 = num2 - 1;
+    Console.WriteLine(num3);
+}
+if (num1 <= 1){
+    Console.WriteLine("Четных чисел нет");
+}
