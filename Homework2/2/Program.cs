7c344c210cdfcf1e0ddc9c6ff16b8a0b6b258b48
Dima <d3i9m9a2015s@gmail.com>
2023-09-05 20:54:33 +0900

    дз2 доделал

--- Homework2/2/Program.cs
+++ Homework2/2/Program.cs
@@ -9,7 +9,34 @@ if (num1 < 99){
   Console.WriteLine("Третьей цифры нет!");
 }
   else {
-int num2 = num1/10;
-int num3 = num2%10;
-Console.WriteLine("Третья цифра вашего числа: " + num3);
-  }
+    if (num1 >=100 && num1 < 999){
+       int num2 = num1;
+       num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+    }
+    if (num1>999 && num1<10000){
+      int num2 = num1/10;
+      num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+    } 
+    if (num1>9999 && num1<100000){
+      int num2 = num1/100;
+      num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+      }
+      if (num1>99999 && num1<1000000){
+      int num2 = num1/1000;
+      num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+      }
+      if (num1>999999 && num1<10000000){
+      int num2 = num1/10000;
+      num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+      }
+      if (num1>9999999 && num1<100000000){
+      int num2 = num1/100000;
+      num2 = num2%10;
+      Console.WriteLine("Третья цифра вашего числа: "+num2);
+      }
+    }
