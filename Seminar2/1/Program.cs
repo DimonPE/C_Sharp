dd73b450b0842be5aa6ad5bbc073b1c3e2c89910
Dima <d3i9m9a2015s@gmail.com>
2023-09-05 18:20:45 +0900

    1 задача семинара

---
+++ Seminar2/1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Генерируется случайное число от 10 до 99 и показывает наибольшую его цифру.
+
+Random rand = new Random();
+int num = rand.Next(10,100);
+int num1 = num / 10;
+int num2 = num % 10;
+if (num1 > num2){
+    Console.Write("Выпало число: " + (num) + " и большая его цифра: " + (num1));
+}
+else{
+    Console.Write("Выпало число: " + (num) + " и большая его цифра: " + (num2));
+}
